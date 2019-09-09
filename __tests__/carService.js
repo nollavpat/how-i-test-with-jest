26ff9215233d2e1d805244b3da7897799d887391
Jairus Patrick Vallon <jairus@ubx.ph>
2019-09-09 15:21:47 +0800

    Update naming hehe

--- __tests__/carService.js
+++ __tests__/carService.js
@@ -6,18 +6,18 @@ jest.mock('../knexFile', () => ({}));
 describe('carService', () => {
   describe('#getCarById()', () => {
     test('validates the parameter of select, from and where', async () => {
-      const whereStub = jest.fn(async () => 'car object');
-      const fromStub = jest.fn(() => ({
-        where: whereStub,
+      const whereMock = jest.fn(async () => 'car object');
+      const fromMock = jest.fn(() => ({
+        where: whereMock,
       }));
-      const selectStub = jest.fn(() => ({from: fromStub}));
-      knex.select = selectStub;
+      const selectMock = jest.fn(() => ({from: fromMock}));
+      knex.select = selectMock;
 
       const car = await carService.getCarById(1);
 
-      expect(selectStub).toHaveBeenCalledWith();
-      expect(fromStub).toHaveBeenCalledWith('cars');
-      expect(whereStub).toHaveBeenCalledWith({id: 1});
+      expect(selectMock).toHaveBeenCalledWith();
+      expect(fromMock).toHaveBeenCalledWith('cars');
+      expect(whereMock).toHaveBeenCalledWith({id: 1});
       expect(car).toBe('car object');
 
       delete knex.select;
